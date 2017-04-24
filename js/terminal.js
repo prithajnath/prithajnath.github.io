f2362f43834f47b5c5ee327b4f0891106f44c1fa
Prithaj <prithajnath@gmail.com>
2017-04-24 04:13:17 +0000

    Add cat command

--- js/terminal.js
+++ js/terminal.js
@@ -1,5 +1,15 @@
 /*global KeyCodes*/
 
+
+let Info = {
+    "about.txt":`I am Prithaj, a CS student at SUNY Plattsburgh. I love web development and dank
+    memes`,
+    "education.txt":"Computer Science,BS, SUNY Plattsburgh",
+    "experience.txt":"Jr. Web Developer, End Point Corp: May - Aug 2016",
+    "languages.txt":"Python,Ruby,HTML,CSS and JavaScript"
+}
+
+
 let KeyCodes = {
     81:"q",
     87:"w",
@@ -39,15 +49,19 @@ let KeyCodes = {
 };
 
 let commands = {
-    "ls":()=>{
+    "ls":(x)=>{
         displayOutput("about.txt education.txt  experience.txt languages.txt");
     },
-    "clear":()=>{
+    "clear":(x)=>{
         let terminal = document.getElementById("terminal");
         let bash = document.getElementById("bash");
         terminal.innerHTML = "";
         terminal.appendChild(bash);
         document.getElementById("input").innerHTML = "";
+    },
+    "cat":(x)=>{
+        let info = x.replace("cat ","");
+        displayOutput(Info[info]);
     }
     
 }
@@ -77,8 +91,8 @@ document.addEventListener('keydown', function(event) {
      let x = document.getElementById("input");
      key = event.keyCode;
      if(key == 13){
-         if (commands[x.innerHTML]!=undefined){
-             commands[x.innerHTML]();
+         if (commands[x.innerHTML.split(" ")[0]]!=undefined){
+             commands[x.innerHTML.split(" ")[0]](x.innerHTML);
          }else{
              errorMessage(x);
          }
