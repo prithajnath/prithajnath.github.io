f07ed5fd231b99b438f713005ac1e60288babf6e
Prithaj <prithajnath@gmail.com>
2017-05-05 03:42:46 +0000

    Migrate KeyCodes to Firebase

--- js/dataStore.js
+++ js/dataStore.js
@@ -5,57 +5,24 @@ let fb = require('./firebase');
 let Info = {};
 //Files data structure
 let files = [];
+// Key mappings data structure
+let KeyCodes = {};
 
 fb.firebase.database().ref().once('value').then(function(snapshot) {
   let fbaseObj = snapshot.val();
   let infoKeys = Object.keys(fbaseObj['Info']);
+  let mapKeys = Object.keys(fbaseObj['KeyCodes']);
   for(let i=0;i<infoKeys.length;i++){
       Info[infoKeys[i].replace("_",".")] = fbaseObj['Info'][infoKeys[i]];
       files.push(infoKeys[i].replace("_","."));
   }
+  
+  for(let j=0;j<mapKeys.length;j++){
+      KeyCodes[mapKeys[j]] = fbaseObj['KeyCodes'][mapKeys[j]];
+  }
 }, function(error){
     console.log("Error occured",error);
 });
-
-
-//Key Mappings
-let KeyCodes = {
-    81:"q",
-    87:"w",
-    69:"e",
-    82:"r",
-    84:"t",
-    89:"y",
-    85:"u",
-    73:"i",
-    79:"o",
-    80:"p",
-    219:"[",
-    221:"]",
-    220:"\\",
-    65:"a",
-    83:"s",
-    68:"d",
-    70:"f",
-    71:"g",
-    72:"h",
-    74:"j",
-    75:"k",
-    76:"l",
-    186:";",
-    222:"'",
-    90:"z",
-    88:"x",
-    67:"c",
-    86:"v",
-    66:"b",
-    78:"n",
-    77:"m",
-    188:",",
-    190:".",
-    191:"/",
-    32:" "
-};
 
 module.exports = {
     files: files,
