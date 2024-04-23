a0dcfa6942eb091db99ad8167673796c012fb7ce
Prithaj Nath <prithajnath@gmail.com>
2024-04-23 18:02:31 -0400

    Save dark mode config to localStorage

--- js/functions.js
+++ js/functions.js
@@ -1,136 +1,142 @@
 let $ = require("jquery");
 
-
-
 // Check if args present in command
-function checkIfArgs(a){
-    return a.filter(x => x.indexOf("--")>-1).length>0 ? true : false;
-    
+function checkIfArgs(a) {
+  return a.filter((x) => x.indexOf("--") > -1).length > 0 ? true : false;
 }
-
 
 // Check if string a is a prefix of string b
-function IfPrefix(a,b){
-    let isPrefix=true;
-    for(let i=0;i<a.length;i++){
-        if(a[i]!=b[i]){
-            isPrefix=false;
-            break;
-        }
-        
+function IfPrefix(a, b) {
+  let isPrefix = true;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) {
+      isPrefix = false;
+      break;
     }
-     return isPrefix;
-};
+  }
+  return isPrefix;
+}
 
 // return path
-function printPath(){
-    return path ? path : "/"; /*global path*/
+function printPath() {
+  return path ? path : "/"; /*global path*/
 }
-
 
 // return context
-function getContext(files){
-    var keys = path.split("/").slice(1);
-    var current_key;
-    for(var k=0;k<keys.length;k++){
-        if(current_key){
-            current_key = current_key[keys[k]];
-        }else{
-            current_key = files[keys[k]];
-        }
-        
+function getContext(files) {
+  var keys = path.split("/").slice(1);
+  var current_key;
+  for (var k = 0; k < keys.length; k++) {
+    if (current_key) {
+      current_key = current_key[keys[k]];
+    } else {
+      current_key = files[keys[k]];
     }
-    
-    return current_key ? current_key : files;
-    
+  }
+
+  return current_key ? current_key : files;
 }
 
-
 // Create new bash node
-function createNewBash(){
-    // create DOM elements
-    let newBash = document.createElement("DIV");
-    let mainP = document.createElement("P");
-    let input = document.createElement("SPAN");
-    let blinker = document.createElement("SPAN");
-    
-    // set sttributes to created elements
-    mainP.innerHTML="~$";
-    newBash.setAttribute("id","bash")
-    input.setAttribute("id","input");
-    blinker.setAttribute("id","blinker");
-    blinker.innerHTML="_";
-    
-    // Append nodes
-    mainP.appendChild(input);
-    mainP.appendChild(blinker);
-    newBash.appendChild(mainP);
-    
-    return newBash;
-    
+function createNewBash() {
+  // create DOM elements
+  let newBash = document.createElement("DIV");
+  let mainP = document.createElement("P");
+  let input = document.createElement("SPAN");
+  let blinker = document.createElement("SPAN");
+
+  // set sttributes to created elements
+  mainP.innerHTML = "~$";
+  newBash.setAttribute("id", "bash");
+  input.setAttribute("id", "input");
+  blinker.setAttribute("id", "blinker");
+  blinker.innerHTML = "_";
+
+  // Append nodes
+  mainP.appendChild(input);
+  mainP.appendChild(blinker);
+  newBash.appendChild(mainP);
+
+  return newBash;
 }
 
 // Remove attributes from old bash node WARNING: potentially dangerous
-function removeOldBash(){
-    document.getElementById("blinker").innerHTML = "";
-    document.getElementById("blinker").setAttribute("id","");
-    document.getElementById("input").setAttribute("id","");
-    document.getElementById("bash").setAttribute("id","");
+function removeOldBash() {
+  document.getElementById("blinker").innerHTML = "";
+  document.getElementById("blinker").setAttribute("id", "");
+  document.getElementById("input").setAttribute("id", "");
+  document.getElementById("bash").setAttribute("id", "");
 }
 
-
 // Print data to the DOM
-function displayOutput(output){
-    let elem = document.createElement("P");
-    let terminal = document.getElementById("terminal");
-    
-    // Using jQuery. Finally gave up
-    $( elem ).html(() => output.replace( /;/g, "<br/><br/>") );
-    
-    
-    // new bash node
-    let NewTerminal = createNewBash();
-    
-    // remove old bash
-    removeOldBash();
-    
-    // Append nodes
-    terminal.appendChild(elem);
-    terminal.appendChild(NewTerminal);
-    
-    return elem;
+function displayOutput(output) {
+  let elem = document.createElement("P");
+  let terminal = document.getElementById("terminal");
 
-};
-    
+  // Using jQuery. Finally gave up
+  $(elem).html(() => output.replace(/;/g, "<br/><br/>"));
+
+  // new bash node
+  let NewTerminal = createNewBash();
+
+  // remove old bash
+  removeOldBash();
+
+  // Append nodes
+  terminal.appendChild(elem);
+  terminal.appendChild(NewTerminal);
+
+  return elem;
+}
+
 // Print error message in DOM
-function errorMessage(x){
-    displayOutput("bash: "+x.innerHTML+": command not found");
-};
+function errorMessage(x) {
+  displayOutput("bash: " + x.innerHTML + ": command not found");
+}
 
 // Switch modes (light and dark)
-function changeColor(){
-    let colors = {"rgb(255, 255, 255)":"rgb(51, 51, 51)","rgb(51, 51, 51)":"rgb(255, 255, 255)"};
-    let bulb = {"brightness(1) invert(0)":"brightness(0) invert(1)","brightness(0) invert(1)":"brightness(1) invert(0)"};
-    if(document.body.style.backgroundColor!="" && document.body.style.color!="" && document.getElementById("bulb").style.filter!=""){
-        document.body.style.backgroundColor = colors[document.body.style.backgroundColor];
-        document.body.style.color = colors[document.body.style.color];
-        document.getElementById("bulb").style.filter = bulb[document.getElementById("bulb").style.filter];
-        
-}else{
+function changeColor(bg = null, bgColor = null, btnColor = null) {
+  let colors = {
+    "rgb(255, 255, 255)": "rgb(51, 51, 51)",
+    "rgb(51, 51, 51)": "rgb(255, 255, 255)",
+  };
+  let bulb = {
+    "brightness(1) invert(0)": "brightness(0) invert(1)",
+    "brightness(0) invert(1)": "brightness(1) invert(0)",
+  };
+  if (bg !== null && bgColor !== null && btnColor !== null) {
+    document.body.style.backgroundColor = colors[bg];
+    document.body.style.color = colors[bgColor];
+    document.getElementById("bulb").style.filter = btnColor;
+  } else if (
+    document.body.style.backgroundColor != "" &&
+    document.body.style.color != "" &&
+    document.getElementById("bulb").style.filter != ""
+  ) {
+    document.body.style.backgroundColor =
+      colors[document.body.style.backgroundColor];
+    document.body.style.color = colors[document.body.style.color];
+    document.getElementById("bulb").style.filter =
+      bulb[document.getElementById("bulb").style.filter];
+
+    localStorage.setItem("bulb", document.getElementById("bulb").style.filter);
+    localStorage.setItem("bgColor", document.body.style.backgroundColor);
+    localStorage.setItem("bg", document.body.style.color);
+  } else {
     document.body.style.backgroundColor = "#fff";
     document.body.style.color = "#333";
     document.getElementById("bulb").style.filter = "brightness(1) invert(0)";
-    }
-};
+  }
+}
 
 module.exports = {
-    errorMessage: errorMessage,
-    removeOldBash: removeOldBash,
-    createNewBash: createNewBash,
-    displayOutput: displayOutput,
-    IfPrefix: IfPrefix,
-    changeColor: changeColor,
-    printPath: printPath,
-    getContext: getContext,
-    checkIfArgs: checkIfArgs
-}
+  errorMessage: errorMessage,
+  removeOldBash: removeOldBash,
+  createNewBash: createNewBash,
+  displayOutput: displayOutput,
+  IfPrefix: IfPrefix,
+  changeColor: changeColor,
+  printPath: printPath,
+  getContext: getContext,
+  checkIfArgs: checkIfArgs,
+};
--- js/terminal.js
+++ js/terminal.js
@@ -1,12 +1,11 @@
 require("babel-core/register");
 require("babel-polyfill");
 
-
 // loading data with Promise
-var dStore = {}
-require("./dataStore.js").then((x)=>{
-    dStore = x.val();
-    dStore['files'] = Object.keys(dStore['Info']).map(x => x.replace("_","."));
+var dStore = {};
+require("./dataStore.js").then((x) => {
+  dStore = x.val();
+  dStore["files"] = Object.keys(dStore["Info"]).map((x) => x.replace("_", "."));
 });
 
 let func = require("./functions.js");
@@ -15,8 +14,7 @@ let $ = require("jquery");
 
 // global path variable
 path = "/"; /*global path*/
-current_dir = "/" /*global current_dir*/
-
+current_dir = "/"; /*global current_dir*/
 
 // Command caching
 let commandStack = [];
@@ -26,87 +24,103 @@ let commandIndex = 0;
 let autoCompleteCache = [];
 let autoCompleteCount = 0;
 
-document.addEventListener('keydown', function(event) {
-     let x = document.getElementById("input");
-     let key = event.keyCode;
-     // Enter key
-     if(key === 13){
-         if (cmds.commands[x.innerHTML.split(" ")[0]]!=undefined){
-             cmds.commands[x.innerHTML.split(" ")[0]](x.innerHTML);
-             if(x.innerHTML!=""){
-                 commandStack.push(x.innerHTML);
-             }else{
-                 commandStack.push("clear");
-             }
-             commandIndex = commandStack.length;
-         }else if(x.innerHTML.split(" ")[0]===""){
-             func.displayOutput("");
-         }
-         else{
-             func.errorMessage(x);
-         }
-         autoCompleteCache = [];
-         autoCompleteCount = 0;
+document.addEventListener("keydown", function (event) {
+  let x = document.getElementById("input");
+  let key = event.keyCode;
+  // Enter key
+  if (key === 13) {
+    if (cmds.commands[x.innerHTML.split(" ")[0]] != undefined) {
+      cmds.commands[x.innerHTML.split(" ")[0]](x.innerHTML);
+      if (x.innerHTML != "") {
+        commandStack.push(x.innerHTML);
+      } else {
+        commandStack.push("clear");
+      }
+      commandIndex = commandStack.length;
+    } else if (x.innerHTML.split(" ")[0] === "") {
+      func.displayOutput("");
+    } else {
+      func.errorMessage(x);
+    }
+    autoCompleteCache = [];
+    autoCompleteCount = 0;
 
-         // scrolling
-         if(document.body.scrollHeight-document.getElementById("terminal").offsetHeight<300){
-             $("html, body").animate({ scrollTop: $(document).height()-$(window).height() }, 60);
-         }
-     }
+    // scrolling
+    if (
+      document.body.scrollHeight -
+        document.getElementById("terminal").offsetHeight <
+      300
+    ) {
+      $("html, body").animate(
+        { scrollTop: $(document).height() - $(window).height() },
+        60
+      );
+    }
+  }
 
-     // Up arrow key
-     if(key === 38){
-         event.preventDefault();
-         if(commandIndex-1>=0){
-             commandIndex -=1;
-             x.innerHTML = commandStack[commandIndex];
-         }
-     }
-     // Down arrow key
-     if(key === 40){
-         if(commandIndex+1<commandStack.length){
-             commandIndex +=1;
-             x.innerHTML = commandStack[commandIndex];
-         }else if(commandIndex===commandStack.length-1){
-             x.innerHTML = "";
-             commandIndex +=1;
-         }
-     }
-     // Backspace key
-     if(key === 8){
-         event.preventDefault();
-         x.innerHTML = x.innerHTML.slice(0,x.innerHTML.length-1);
-     }else if(dStore.KeyCodes[key]!= undefined){
-         x.innerHTML = x.innerHTML + dStore.KeyCodes[key];
-     }
-     // Tab key
-     if(key === 9){
-         event.preventDefault();
-         let inputArray = x.innerHTML.split(" ");
-         console.log(inputArray);
-         console.log(func.checkIfArgs(inputArray));
-         if(func.checkIfArgs(inputArray)){
-             var [command, arg, file] = inputArray;
-         }else{
-             var [command, file] = inputArray;
-             var arg = "";
-         }
-         console.log(file);
-         //let command = x.innerHTML.split(" ")[0];
-         //let file = x.innerHTML.split(" ").slice(-1)[0];
-         let files = Object.keys(func.getContext(dStore["Info"])).map(x => x.replace("_","."));
-         if (autoCompleteCache.length===0){
-            for(let i=0;i<files.length;i++){
-                if(func.IfPrefix(file,files[i])){
-                 autoCompleteCache.push(files[i]);
-                }
-            }
-         }
-         if(autoCompleteCache[autoCompleteCount]){
-             x.innerHTML = arg ? command+" "+arg+" "+autoCompleteCache[autoCompleteCount] : command+" "+autoCompleteCache[autoCompleteCount];
-            autoCompleteCount = (autoCompleteCount + 1)%autoCompleteCache.length;   
-         }
-     }
+  // Up arrow key
+  if (key === 38) {
+    event.preventDefault();
+    if (commandIndex - 1 >= 0) {
+      commandIndex -= 1;
+      x.innerHTML = commandStack[commandIndex];
+    }
+  }
+  // Down arrow key
+  if (key === 40) {
+    if (commandIndex + 1 < commandStack.length) {
+      commandIndex += 1;
+      x.innerHTML = commandStack[commandIndex];
+    } else if (commandIndex === commandStack.length - 1) {
+      x.innerHTML = "";
+      commandIndex += 1;
+    }
+  }
+  // Backspace key
+  if (key === 8) {
+    event.preventDefault();
+    x.innerHTML = x.innerHTML.slice(0, x.innerHTML.length - 1);
+  } else if (dStore.KeyCodes[key] != undefined) {
+    x.innerHTML = x.innerHTML + dStore.KeyCodes[key];
+  }
+  // Tab key
+  if (key === 9) {
+    event.preventDefault();
+    let inputArray = x.innerHTML.split(" ");
+    console.log(inputArray);
+    console.log(func.checkIfArgs(inputArray));
+    if (func.checkIfArgs(inputArray)) {
+      var [command, arg, file] = inputArray;
+    } else {
+      var [command, file] = inputArray;
+      var arg = "";
+    }
+    console.log(file);
+    //let command = x.innerHTML.split(" ")[0];
+    //let file = x.innerHTML.split(" ").slice(-1)[0];
+    let files = Object.keys(func.getContext(dStore["Info"])).map((x) =>
+      x.replace("_", ".")
+    );
+    if (autoCompleteCache.length === 0) {
+      for (let i = 0; i < files.length; i++) {
+        if (func.IfPrefix(file, files[i])) {
+          autoCompleteCache.push(files[i]);
+        }
+      }
+    }
+    if (autoCompleteCache[autoCompleteCount]) {
+      x.innerHTML = arg
+        ? command + " " + arg + " " + autoCompleteCache[autoCompleteCount]
+        : command + " " + autoCompleteCache[autoCompleteCount];
+      autoCompleteCount = (autoCompleteCount + 1) % autoCompleteCache.length;
+    }
+  }
 });
 
-document.getElementById("bulb").addEventListener("click",func.changeColor);
+document.getElementById("bulb").addEventListener("click", func.changeColor);
+document.addEventListener("DOMContentLoaded", function (e) {
+  const btnColor = localStorage.getItem("bulb");
+  const bgColor = localStorage.getItem("bgColor");
+  const bg = localStorage.getItem("bg");
+  func.changeColor(bg, bgColor, btnColor);
+});
